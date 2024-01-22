6a363264be3c4259f61c078416c839b60c3e6d45
sultanparvez <sultanparvez77.sp@gmail.com>
2024-01-23 01:08:20 +0600

    Added Test02

--- src/test/java/tests/TestSuite01.java
+++ src/test/java/tests/TestSuite01.java
@@ -1,14 +1,16 @@
 package tests;
 
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class TestSuite01 {
     @Test
     public  void test_1(){
-        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        Response response = get("https://reqres.in/api/users?page=2");
         System.out.println(response.getStatusCode());
         System.out.println(response.getTime());
         System.out.println(response.getBody().asString());
@@ -16,7 +18,15 @@ public class TestSuite01 {
         System.out.println(response.getHeader("content-type"));
 
         int statusCode = response.statusCode();
-        Assert.assertEquals(statusCode,201);
-
+        Assert.assertEquals(statusCode,200);
+    }
+    @Test
+    public void  test_2(){
+        baseURI ="https://reqres.in/api/";
+        given().get("/users?page=2").
+                then().
+                statusCode(200).
+                body("data[1].id",equalTo(8)).
+                log().all();
     }
 }
