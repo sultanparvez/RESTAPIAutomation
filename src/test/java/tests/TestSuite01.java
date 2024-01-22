d089fe23d52d15a2ec36ada2adc29ddb79a16f32
sultanparvez <sultanparvez77.sp@gmail.com>
2024-01-23 00:44:40 +0600

    Pilot

---
+++ src/test/java/tests/TestSuite01.java
@@ -0,0 +1,22 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSuite01 {
+    @Test
+    public  void test_1(){
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getTime());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusLine());
+        System.out.println(response.getHeader("content-type"));
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode,201);
+
+    }
+}
