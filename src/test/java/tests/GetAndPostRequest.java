f32b47d0902fa295bd82a5da82f17ae2a3cb65a8
sultanparvez <sultanparvez77.sp@gmail.com>
2024-01-25 02:31:07 +0600

    Advanced post request

--- src/test/java/tests/GetAndPostRequest.java
+++ src/test/java/tests/GetAndPostRequest.java
@@ -4,6 +4,8 @@ import io.restassured.http.ContentType;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,25 +24,36 @@ public class GetAndPostRequest {
     }
     @Test
     public void PostAPI(){
-        Map<String, String> map = new HashMap<>();
-        map.put("name","Parvez");
-        map.put("job","Automation Engineer");
-        System.out.println(map);
-        JSONObject requestBody = new JSONObject(map);
-        System.out.println(requestBody);
-        JSONObject request = new JSONObject();
-        request.put("name","Parvez");
-        request.put("job","Automation Engineer");
-        System.out.println(request.toJSONString());
+//        Map<String, String> map = new HashMap<>();
+//        map.put("name","Parvez");
+//        map.put("job","Automation Engineer");
+//        System.out.println(map);
+//        JSONObject requestBody = new JSONObject(map);
+//        System.out.println(requestBody);
+//        JSONObject request = new JSONObject();
+//        request.put("name","Parvez");
+//        request.put("job","Automation Engineer");
+//        System.out.println(request.toJSONString());
+//        Read the content of the JSON file into a String
+        String jsonFilePath = "classpath:bodyData.json";
+        String requestBody = null;
+
+        try {
+            requestBody = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         baseURI ="https://reqres.in";
+        if (requestBody != null) {
         given().header("Content-Type","application/json").
                 contentType(ContentType.JSON).
                 accept(ContentType.JSON).
-                body(request.toJSONString()).
+                body(requestBody).
                 when().
                 post("api/users").
                 then().
                 statusCode(201)
                 .log().all();
+    }
     }
 }
